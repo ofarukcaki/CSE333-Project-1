5062b504c496a33273877fa3ca4895df849f1dfa
O.Faruk <omerfarukcaki@gmail.com>
2019-10-22 16:06:00 +0300

    Update 2.sh
    Final version

--- solutions/2.sh
+++ solutions/2.sh
@@ -2,12 +2,12 @@
 
 if [ "$1" != "" ]
 then
-    mkdir "$1/cprogs"
+    mkdir -p "$1/cprogs" # -p prevents the warnin if folder already exist
     mv *.c "$1/cprogs/"
     echo "A directory named cprogs is created under $1."
     echo "All the files whose name ends with .c in $1 are moved into cprogs directory."
 else
-    mkdir "cprogs"  # create a folder
+    mkdir -p "cprogs"  # create a folder
     mv *.c "./cprogs/" # move all .c files to that folder
     echo "A directory named cprogs is created under current working directory."
     echo "All the files whose name ends with .c in the current working directory are moved into cprogs directory."
