f03a147aff7caea82a28a67c130333b69e6a2103
O.Faruk <omerfarukcaki@gmail.com>
2019-10-27 20:20:37 +0300

    Update 5.sh

--- solutions/1.sh
+++ solutions/1.sh
@@ -3,12 +3,12 @@
 word=$1
 reverse=""
 
-for(( i=0 ; i<${#word} ; i++ ))
+for(( i=0 ; i<${#word} ; i++ ))     # loop through all chars
 do 
-    reverse="${word:i:1}$reverse"
+    reverse="${word:i:1}$reverse"   # combine all characters in reverse order to produce reversed text
 done
 
-if [ "${word^^}" == "${reverse^^}" ]
+if [ "${word^^}" == "${reverse^^}" ] # compare original text's uppercase version with reversed text's uppercase v.
 then
     echo "$word is a palindrome"
 else
--- solutions/5.sh
+++ solutions/5.sh
@@ -2,63 +2,69 @@
 
 txt_files=""
 path="./"
+recursive=0
 
-if [ "$2" != "" ]
+# There are 4 possible cases
+# $1 = -R
+# $1 = -R and $2 = path
+# $1 = path
+# $1 = path and $2 = -R
+
+if [ "$2" == "-R" ]
 then
+    # 4th case
+    recursive=1
+    path=$1
+elif [ "$1" == "-R" ] && [ "$2" == "" ]
+then
+    # 1st case
+    recursive=1
+elif [ "$1" == "-R" ] && [ "$2" != "" ]
+then
+    # 2nd case
+    recursive=1
     path=$2
+elif [ "$2" == "" ] && [ "$1" != "-R" ]
+then
+    # 3rd case
+    path=$1
+else
+    echo "There is problem with arguments."
 fi
 
-if [ "$1" == "-R" ]
+
+if [ "$recursive" == "1" ]  # if -R option is provided
 then
-    txt_files=`find $path -name "*.txt"`;
+    txt_files=`find $path -name "*.txt"`;   # get all .txt files including subfolders and store them in txt_files variable
 else
-    txt_files=`find -maxdepth 1 -name "*.txt"`;
+    txt_files=`find -maxdepth 1 -name "*.txt"`; # get all .txt files in current dir and store them in txt_files variable
 fi
+
 
 # loop through .txt files in directory
-
-
 for file in $txt_files
 do
-    size=`stat --printf="%s" $file`
+    size=`stat --printf="%s" $file` # get the size of the file
     if [ $size -eq 0 ]
     then
         retry=1
-        while [ $retry -eq 1 ]
+        while [ $retry -eq 1 ]  # loop until a valid option is selected
         do
             echo "Do you want to delete $file (y/n)"
             read ans
             if [ $ans == "y" ] || [ $ans == "Y" ]
             then
-                # sil
+                # delete
                 rm $file 
                 echo "$file has been deleted."
                 retry=0
             elif [ $ans == "n" ] || [ $ans == "N" ]
             then 
                 retry=0
-                # nothing
+                # do nothing
             else
                 echo "You should enter either y or n"
             fi
         done      
     fi
 done
-# while read -r file; do
-#     size=`stat --printf="%s" $file`
-#     if [ $size -eq 0 ]
-#     then
-#         echo "Do you want to delete $file"
-#         read ans
-#         echo "answer > $ans"
-#         # read answer
-#     fi
-# done <<< "$txt_files"
-
-
-# if path & name
-
-
-# if path only
-
-# if name only
