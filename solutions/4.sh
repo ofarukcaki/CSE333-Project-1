3451ac032815eb0161d5f28b03c1d66a92c12a39
O.Faruk <omerfarukcaki@gmail.com>
2019-10-31 22:54:07 +0300

    Update 4.sh: perl

--- solutions/4.sh
+++ solutions/4.sh
@@ -11,11 +11,17 @@ originalWildcard=$1
 regex=`echo $1 | sed 's/*/\\\w*/g'`
 # loop through .txt files in directory
 while read -r line; do
-    matched=`perl -lane "print m/(\s$regex\s)/g" $line`
+    # perl -nle '@a = /\W*(\w*very)\w*/g; $,=","; print @a' test.txt # comma-seperated
+    matched=`perl -nle "@a = /\W*($regex)\w*/g; $,=','; print @a" $line`
+    echo "Found: $a"
+    # matched=`perl -lane "@a = m/(\s$regex)\W{0,1}/g; $,=","; print @a" $line`
+
+    # FIXME: loop through the comma-seperated "matched"
     for value in $matched # loop through all matched results
     do
-        
-        echo "$value"
+        echo "The word \"$value\" inside $line is substituted with ${value^^}."
+        value=" $value"
+        # `sed -i "s/$value/${value^^}/g" $line`
     done
 done <<< "$txt_files"
 
---
+++ tests/reg.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+a="asd severy" =~ /\w*very/g
+
+echo $a
